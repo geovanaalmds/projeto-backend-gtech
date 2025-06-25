b80db4051300bd1b7d9b934aa5674c7adfb0324f
geovanaalmds <geovanaalmds@outlook.com>
2025-06-25 20:20:47 -0300

    att rota de produtos

--- src/routes/ProductsRoutes.js
+++ src/routes/ProductsRoutes.js
@@ -3,3 +3,11 @@ const ProductsController = require('../controllers/ProductsController');
 const ProductRoutes = express.Router();
 
 const productController = new ProductsController();
+
+ProductRoutes.get('/search', productController.search);
+ProductRoutes.get('/:id', productController.getProductById);
+ProductRoutes.post('/', productController.create);
+ProductRoutes.put('/:id', productController.update);
+ProductRoutes.delete('/:id', productController.delete);
+
+module.exports = ProductRoutes;
